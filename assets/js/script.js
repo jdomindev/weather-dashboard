878cb31b6e0aa76064bca2969fa99b419753a9a1
Jose Dominguez <jdomindev@gmail.com>
2021-12-19 00:06:18 -0500

    added more html and utilized bootstrap for some styles, also added more javascript to get specific data from weather web api

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,7 @@ function searchCity(event) {
   if (citySubmitForm === null) {
     console.log("NULL");
   } else {
-    var currentCitySearch = citySearch.value;
-    var city = currentCitySearch.toLowerCase();
+    var city = citySearch.value;
     var apiKey = "appid=13ac4c2d8bb2ff4c49a57881f07758ad";
     var requestUrlGeocode =
       "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -21,7 +20,8 @@ function searchCity(event) {
         return response.json();
       })
       .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
           var longitude = data[i].lon;
           var latitude = data[i].lat;
         }
@@ -41,12 +41,62 @@ function searchCity(event) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          console.log(data.current.temp);
-        }
+        // get data for current weather
+        console.log(data.current);
+        var unixDate = data.current.dt
+        var iconCode = data.current.weather[0].icon
+        var iconSrc = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        var currentTemp = data.current.temp;
+        var currentWind = data.current.wind_speed;
+        var currentHumid = data.current.humidity;
+        var currentUV = data.current.uvi;
+        // add current weather to document 
+        var currentCity = document.querySelector(".current-city");
+        currentCity.append(city);
+        // Need to convert unix to regular
+        var currentCity = document.querySelector(".current-date");
+        currentCity.append();
+        var weatherIconCurrent = document.querySelector(".weather-icon")
+        weatherIconCurrent.setAttribute("src", iconSrc)
+        var currentTempEl = document.querySelector("#current-temp");
+        var currentWindEl = document.querySelector("#current-wind");
+        var currentHumidEl = document.querySelector("#current-humid");
+        var currentUVEl = document.querySelector("#current-uv");
+        currentTempEl.append("Temperature: " + currentTemp + " F");
+        currentWindEl.append("Wind speed: " + currentWind + " MPH");
+        currentHumidEl.append("Humidity: " + currentHumid + "%");
+        currentUVEl.append("UV Index: " + currentUV);
+        // make arrays for daily weather
+        console.log(data.daily);
+        var dailyWeather = []
+        var dailyTemps = []
+        var dailyWind = []
+        var dailyHumid = []
+        // get data for daily weather
+        for (let i = 0; i < 5; i++) {
+          dailyWeather.push(data.daily[i].weather[0].icon)
+          dailyTemps.push(data.daily[i].temp.day);
+          dailyWind.push(data.daily[i].wind_speed);
+          dailyHumid.push(data.daily[i].humidity);
+          var unixNum = data.daily[0].dt
+          var unixString = unixNum.toString()
+        } 
+        // console.log(unixString);
+        console.log(moment.unix(unixString).format("MMMM Do, YYYY"));
       });
   }
 }
 
+// function addContent() {
+// }
+
 citySubmitForm.addEventListener("submit", searchCity);
+
+
+// Left to do:
+// need to rework css 
+
+// in script:
+// need to convert unix timestamp to date format
+// needs to clear when searching another result
+// break down big function into smaller bits if possible

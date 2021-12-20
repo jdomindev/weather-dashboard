e0e800678fe97194a083b36cfcbd017276b4340b
Jose Dominguez <jdomindev@gmail.com>
2021-12-19 21:56:23 -0500

    added more styling using bootstrap, and more script to include a better way to call city name and have search history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+    // WHEN I view current weather conditions for that city
+  // X THEN I am presented with the city name, the date, an icon representation of weather conditions, the        temperature, the humidity, the wind speed, and the UV index
+    // WHEN I view the UV index
+  // X THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
 var citySubmitButton = document.querySelector("#city-submit-button");
 var citySubmitForm = document.querySelector("#city-submit-form");
 var citySearch = document.querySelector("#city-search");
@@ -24,6 +36,19 @@ function searchCity(event) {
           var longitude = data[i].lon;
           var latitude = data[i].lat;
         }
+        // current city name
+        var currentCity = data[0].name
+        var currentState = data[0].state
+        var cityEl = document.querySelector(".current-city");
+        cityEl.append(currentCity);
+        cityEl.append(", " + currentState);
+        var searchedCity = document.querySelector(".searched-city")
+        var searchedEl = document.createElement("button")
+        searchedEl.classList.add("btn","btn-secondary","w-100","mt-2")
+        searchedEl.append(currentCity)
+        searchedCity.append(searchedEl)
+        // add event listener for searchedEl
+
         var requestUrl =
           "https://api.openweathermap.org/data/2.5/onecall?" +
           "lat=" +
@@ -50,29 +75,27 @@ function searchCity(event) {
         var currentHumid = data.current.humidity;
         var currentUV = data.current.uvi;
         // add current weather to document
-        var currentCity = document.querySelector(".current-city");
-        currentCity.append(city);
         // convert unix to regular
+        var currentDate = document.querySelector(".current-date");
         var date = moment.unix(currentUnixString).format("l");
-        currentCity.append(" -- " + date);
+        currentDate.append(date);
         var weatherIconCurrent = document.querySelector(".weather-icon");
         weatherIconCurrent.setAttribute("src", iconSrc);
         var currentTempEl = document.querySelector("#current-temp");
         var currentWindEl = document.querySelector("#current-wind");
         var currentHumidEl = document.querySelector("#current-humid");
         var currentUVEl = document.querySelector("#current-uv");
-        currentUVEl.classList.add("class", "uv-background");
         // UV Conditional
         if (currentUV <= 2) {
-          currentUVEl.classList.add("class", "uv-background-low");
+          currentUVEl.classList.add("uv-background","uv-background-low");
         } else if (currentUV > 2 && currentUV < 6) {
-          currentUVEl.classList.add("class", "uv-background-moderate");
+          currentUVEl.classList.add("uv-background","uv-background-moderate");
         } else if (currentUV >= 6 && currentUV < 8) {
-          currentUVEl.classList.add("class", "uv-background-high");
+          currentUVEl.classList.add("uv-background","uv-background-high");
         }else if (currentUV >= 8 && currentUV < 11) {
-          currentUVEl.classList.add("class", "uv-background-very-high");
+          currentUVEl.classList.add("uv-background","uv-background-very-high");
         }else {
-          currentUVEl.classList.add("class", "uv-background-extreme");
+          currentUVEl.classList.add("uv-background","uv-background-extreme");
         }
         currentTempEl.append(currentTemp + " F");
         currentWindEl.append(currentWind + " MPH");
@@ -84,14 +107,29 @@ function searchCity(event) {
         var dailyTemps = [];
         var dailyWind = [];
         var dailyHumid = [];
+        var unixString = []
+
         // get data for daily weather
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 6; i++) {
           dailyWeather.push(data.daily[i].weather[0].icon);
           dailyTemps.push(data.daily[i].temp.day);
           dailyWind.push(data.daily[i].wind_speed);
           dailyHumid.push(data.daily[i].humidity);
-          var unixString = data.daily[0].dt.toString();
+          unixString.push(data.daily[i].dt.toString());
         }
+        
+        // console.log(dailyDate);
+        // var cardTitle = document.querySelector(".card-title");
+        // for (let i = 1; i < unixString.length; i++) {
+        // var dailyDates = unixString[i]
+        // var dailyDate = moment.unix(dailyDates).format("l")
+        // console.log(dailyDates);
+        // console.log(dailyDate);
+        // // cardTitle.append(dailyDate)
+        
+        // }
+        
+  
       });
   }
 }
